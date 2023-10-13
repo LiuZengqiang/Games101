72d26afc67a894bf254938370ab8ce679f40a579
liuzengqiang <12021032@zju.edu.cn>
2023-10-14 03:32:04 +0800

    完成作业3

--- Assignment3/Code/global.hpp
+++ Assignment3/Code/global.hpp
@@ -9,6 +9,7 @@ typedef unsigned char u08;
 #define MY_PI 3.1415926
 #define TWO_PI (2.0* MY_PI)
 
+inline float angle2radian(const float& angle) { return MY_PI / 180.0f * angle; }
 
 
 #endif //RASTERIZER_GLOBAL_H
