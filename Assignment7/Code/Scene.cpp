f4a1e6be285fcaffedae5038e7b0b28181d550e6
liuzengqiang <12021032@zju.edu.cn>
2023-10-16 08:02:22 +0800

    完成作业7

--- Assignment7/Code/Scene.cpp
+++ Assignment7/Code/Scene.cpp
@@ -55,40 +55,31 @@ Vector3f Scene::shade(const Intersection &p, const Vector3f &wo) const {
     return p.m->getEmission();
   }
 
-  // x: 光源区域的采样点 x
-  // ws: 光源的方向 ws(wi)
-  // nn: wi 跟光源区域(面)的法向
-  // emit: 光亮
-  Vector3f L_dir;
-  Vector3f L_indir;
-  Intersection p_light;
-  float pdf_light;
-  sampleLight(p_light, pdf_light);
-
-  Vector3f p_p2light = p_light.coords - p.coords;
-
-  auto t = intersect(Ray(p.coords, p_p2light.normalized()));
-
+  Vector3f L_dir;    // 直接光照
+  Vector3f L_indir;  // 间接光照
+  Intersection x;    // 光源上的采样点
+  float pdf_light;   // 采样x点的概率密度
+  sampleLight(x, pdf_light);
+  Vector3f ws = (x.coords - p.coords).normalized();  // 指向光源采样点x的wi
+  Vector3f NN = x.normal;  // 光线采样点x的法向
+  Vector3f N = p.normal;   // 点p的法向
+  Vector3f emit = x.emit;  // 光源采样点x的亮度
   // 判断light是否被遮挡
-  if (t.distance >= p_p2light.norm() - 0.01) {
-    Vector3f ws = p_p2light.normalized();
-    Vector3f NN = p_light.normal;
-    Vector3f N = p.normal;
-    Vector3f emit = p_light.emit;
+  if ((intersect(Ray(p.coords, ws)).coords - x.coords).norm() <= 0.01) {
     L_dir = emit * p.m->eval(ws, wo, N) * std::max(dotProduct(ws, N), 0.0f) *
             std::max(dotProduct(-ws, NN), 0.0f) /
-            dotProduct(p_p2light, p_p2light) / pdf_light;
+            std::pow((x.coords - p.coords).norm(), 2) / pdf_light;
   }
 
   if (get_random_float() < RussianRoulette) {
-    Vector3f wi = p.m->sample(wo, p.normal).normalized();
-    float pdf_indir = p.m->pdf(wo, wi, p.normal);
+    Vector3f wi = p.m->sample(wo, N).normalized();  // 随机采样wi
+    Intersection q = intersect(Ray(p.coords, wi));  // p向wi方向发射光线的交点q
+    float pdf_indir = p.m->pdf(wo, wi, N);  // 采样方向wi的概率密度
 
     if (pdf_indir > EPSILON) {
-      Intersection q = intersect(Ray(p.coords, wi));
       if (q.happened && q.m->hasEmission() == false) {
-        L_indir = shade(q, -wi) * p.m->eval(wo, wi, p.normal) *
-                  std::max(dotProduct(wi, p.normal), 0.0f) / pdf_indir /
+        L_indir = shade(q, -wi) * p.m->eval(wo, wi, N) *
+                  std::max(dotProduct(wi, N), 0.0f) / pdf_indir /
                   RussianRoulette;
       }
     }
@@ -99,9 +90,6 @@ Vector3f Scene::shade(const Intersection &p, const Vector3f &wo) const {
 // Implementation of Path Tracing
 Vector3f Scene::castRay(const Ray &ray, int depth) const {
   // TO DO Implement Path Tracing Algorithm here
-
-  // pdf(): 计算采样的概率密度值
-  // eval(): 计算光线的 f_r 值
   Intersection inter = intersect(ray);
   if (inter.happened == false) {
     return {0, 0, 0};
